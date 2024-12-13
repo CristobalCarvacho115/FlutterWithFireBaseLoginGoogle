4a49a420edd05949449362dbd6532197240d71d4
CristobalCarvacho115 <cris.carvacho.ponce@gmail.com>
2024-12-13 19:07:04 -0300

    a

--- lib/services/categorias_service.dart
+++ lib/services/categorias_service.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CategoriasService {
-  //Mostrar categorias usado para el dropdown/ComboBox
+  //Mostrar categorias usado para dropdown/ComboBox
   Stream<QuerySnapshot> mostrarCategorias() {
     return FirebaseFirestore.instance.collection('categorias').snapshots();
   }
